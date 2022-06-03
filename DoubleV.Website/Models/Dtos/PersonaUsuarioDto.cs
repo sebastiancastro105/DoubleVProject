9b17eb511b806ad9930cd66758a7e79b1847f55d
Sebastian Castro <juan.castro@3ysolutions.com.co>
2022-06-03 12:31:41 -0500

    Correcciones

--- DoubleV.Website/Models/Dtos/PersonaUsuarioDto.cs
+++ DoubleV.Website/Models/Dtos/PersonaUsuarioDto.cs
@@ -6,23 +6,23 @@ namespace DoubleV.Website.Models.Dtos
     public class PersonaUsuarioDto
     {
         public int Identificador { get; set; }
-        [Required(ErrorMessage ="El campo {0} es requerdio")]
+      
         public string Nombres { get; set; } 
 
-        [Required(ErrorMessage = "El campo {0} es requerdio")]
+        
         public string Apellidos { get; set; }
-        [Required(ErrorMessage = "El campo {0} es requerdio")]
+         [Required]
         public string NumeroIdentificacion { get; set; }
 
-        [DataType(DataType.EmailAddress)]
+       
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "El campo {0} es requerdio")]
+         [Required]
         public string TipoIdentificacion { get; set; }
         public DateTime FechaCreacion { get; set; }
-        [Required(ErrorMessage = "El campo {0} es requerdio")]
+         [Required]
         public string User { get; set; }
-        [Required(ErrorMessage = "El campo {0} es requerdio")]
+        [Required]
         public string Pass { get; set; }
     }
 }
